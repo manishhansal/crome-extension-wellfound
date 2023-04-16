1ec8d6d9da4abe7d8cbae82d95e67753519c45fb
Manish Kumar <manishhansalmeh@gmail.com>
2023-04-16 22:03:28 +0530

    Update

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// Send a message to the background script to replace all instances of "cat" with "dog"
-
-// const replaceButton1 = document.getElementById("replaceButton1");
-// const replaceButton2 = document.getElementById("replaceButton2");
-// const sendMessage1 = () => {
-//   chrome.runtime.sendMessage({ action: "replaceCatsWithDogs" });
-// };
-// const sendMessage2 = () => {
-//     chrome.runtime.sendMessage({ action: "replaceDogsWithCats" });
-// };
-
-// replaceButton1.addEventListener("click", sendMessage1);
-// replaceButton2.addEventListener("click", sendMessage2);
-
 const startButton = document.getElementById("start_btn");
 const handleStart = () => {
   // add id active to start button if not already active

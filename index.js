ebd58e50013e8f7285797a373845d9b0a063f374
Manish Kumar <manishhansalmeh@gmail.com>
2023-04-18 20:54:04 +0530

    Update

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const handleStart = () => {
   // add id active to start button if not already active
   if (!startButton.id.includes("active")) {
     startButton.id = "start_btn_active";
+    startButton.textContent = "Stop";
   }
   // else remove id active from start button
   else {
     startButton.id = "start_btn";
+    startButton.textContent = "Start";
   }
 };
 
